09f4162516a90a3d3d003b5858250c207b15500c
Michael Todd <mtodd@donordrive.com>
2020-08-16 20:11:26 -0500

    Added scroll behavior to router

--- src/router/index.js
+++ src/router/index.js
@@ -48,9 +48,11 @@ const routes = [
 ]
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
+	mode: 'history',
+	routes,
+	scrollBehavior (to, from, savedPosition) {
+		return { x: 0, y: 0 }
+	},
 })
 
 export default router
